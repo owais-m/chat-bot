efcff49900e7ecab30a0f64153f5e15d98ceef45
Owais <44333443+owais-m@users.noreply.github.com>
2024-02-28 19:56:35 +0500

    Update next.py

--- next.py
+++ next.py
@@ -1,10 +1,9 @@
 import openai
-from openai import OpenAI
 import streamlit as st
 
-st.title("ChatGPT-like clone")
-
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+st.title("Jarvis")
+#connect openai key
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -18,17 +17,27 @@ for message in st.session_state.messages:
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-
+    # Display assitant message in chat message container
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
+        message_placeholder = st.empty()
+        full_response = ""
+        # Simulate stream of response with milliseconds delay
+        for response in openai.ChatCompletion.create(
             model=st.session_state["openai_model"],
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
+            #will provide lively writing
             stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        ):
+            #get content in response
+            full_response += response.choices[0].delta.get("content", "")
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
