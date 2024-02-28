a2a35ff64d9a3386d691695da4ec85bee95faac3
Owais <44333443+owais-m@users.noreply.github.com>
2024-02-28 19:58:17 +0500

    Update next.py

--- next.py
+++ next.py
@@ -1,43 +1,26 @@
-import openai
 import streamlit as st
 
-st.title("Jarvis")
-#connect openai key
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+st.title("Echo Bot")
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-
+# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+# React to user input
 if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Display assitant message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        # Simulate stream of response with milliseconds delay
-        for response in openai.ChatCompletion.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            #will provide lively writing
-            stream=True,
-        ):
-            #get content in response
-            full_response += response.choices[0].delta.get("content", "")
-            # Add a blinking cursor to simulate typing
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
+        st.markdown(response)
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    st.session_state.messages.append({"role": "assistant", "content": response})
